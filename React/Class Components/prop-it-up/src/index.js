db7bd02e4ad606378f8ee9770596921d80e5628e
Mohammad-Yasein <myn.shanabli@gmail.com>
2021-01-29 22:26:19 +0200

    prop it up assignment

---
+++ React/Class Components/prop-it-up/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import PersonCard from './PersonCard';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <PersonCard firstName="Jane" lastName="Doe" age="45" hairColor="Black" />
+    <PersonCard firstName="John" lastName="Smith" age="88" hairColor="Brown" />
+    <PersonCard firstName="Millard" lastName="Fillmore" age="50" hairColor="Brown" />
+    <PersonCard firstName="Maria" lastName="Smith" age="62" hairColor="Brown" />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
